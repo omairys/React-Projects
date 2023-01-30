7f220fbf241f63687ae697893798f5cb7ea4b616
omairys <hello.omug@gmail.com>
2023-01-30 17:16:45 -0500

    Building Full-Stack Apps with React and Spring
    Building Full-Stack Apps with React and Spring

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React-Projects
+
+1）crm: Building Full-Stack Apps with React and Spring
---
+++ React-Spring-Projects/crm/src/main/java/com/crm/crm/model/DemoLoader.java
@@ -0,0 +1,22 @@
+package com.crm.crm.model;
+
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoLoader implements CommandLineRunner {
+    
+    private final ContactRepository repository;
+
+    @Autowired
+    public DemoLoader(ContactRepository repository){
+        this.repository =  repository;
+    }
+
+    @Override
+    public void run(String... string)throws Exception {
+        this. repository.save(new Contact("Omairys", "Uzcategui", "me@me.com"));
+    }
+}
